f183edabd4a1018dbe78339bdcaf5c7fe54a96fb
aliceylan2841-web <aliceylan2841@gmail.com>
2026-06-01 12:18:55 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,21 @@
 const CACHE_NAME = 'mesai-takip-v3';
 const ASSETS = [
-  '/mesai_takip/',
-  '/mesai_takip/index.html',
-  '/mesai_takip/manifest.json',
-  '/mesai_takip/icons/icon-192x192.png',
-  '/mesai_takip/icons/icon-512x512.png'
+  '/Mesai-program-v2/',
+  '/Mesai-program-v2/index.html',
+  '/Mesai-program-v2/manifest.json',
+  '/Mesai-program-v2/icons/icon-192x192.png',
+  '/Mesai-program-v2/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS).catch(() => {});
-    })
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS).catch(()=>{})));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+  ));
   self.clients.claim();
 });
 
@@ -29,12 +23,12 @@ self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(cached => {
       if (cached) return cached;
-      return fetch(e.request).then(response => {
-        if (!response || response.status !== 200 || e.request.method !== 'GET') return response;
-        let clone = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
-        return response;
-      }).catch(() => caches.match('/mesai_takip/index.html'));
+      return fetch(e.request).then(res => {
+        if (!res || res.status !== 200 || e.request.method !== 'GET') return res;
+        let clone = res.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+        return res;
+      }).catch(() => caches.match('/Mesai-program-v2/index.html'));
     })
   );
 });
